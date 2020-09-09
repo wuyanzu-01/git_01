d93df2dcfba20471ea6c8b813b6972578aa0b6bb
wuyanzu-01 <320954540@qq.com>
2020-09-09 17:01:12 +0800

    修改了hello

--- git_01/src/main/java/com/dog/Hello.java
+++ git_01/src/main/java/com/dog/Hello.java
@@ -10,6 +10,8 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("das");
+        System.out.println("dfs");
+        System.out.println("cvx");
     }
 
 }
