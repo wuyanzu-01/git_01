30f4a8ccc6339dd12dcefa0678db7e0a95f117de
wuyanzu-01 <320954540@qq.com>
2020-09-09 16:49:44 +0800

    创建了一个项目

---
+++ git_01/src/test/java/com/dog/Git01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
